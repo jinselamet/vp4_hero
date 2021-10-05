36c1fc746d1660122e2572303913b91038ab6cea
jinselamet <77917746+jinselamet@users.noreply.github.com>
2021-10-05 11:20:45 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p /Desktop
+cat << EOF >  /Desktop/google-chrome.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Chromium
+Comment=Access the Internet
+Exec=/usr/bin/google-chrome --no-sandbox %U
+Icon=google-chrome
+Path=
+Terminal=false
+StartupNotify=true
+EOF
+
+chmod +x /Desktop/google-chrome.desktop
+exec supervisord -c /app/supervisord.conf
